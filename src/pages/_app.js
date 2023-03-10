17c50688c6502403b3a970586dd6274ba8a9e6f6
Justin Stendara <jnstendara@gmail.com>
2023-03-09 20:45:07 -0600

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+
+    </>
+  )
 }
