1cc4a733e9c2b04e2576f5cab22c624479ed3787
Justin Stendara <jnstendara@gmail.com>
2023-03-18 12:37:37 -0500

    merged with main

--- src/pages/Login/register.js
+++ src/pages/Login/register.js
@@ -23,21 +23,21 @@ function MyVerticallyCenteredModal(props) {
 <form>
   <div className="form-outline mb-4">
     <input type="email" id="form2Example0" className="form-control" />
-    <label className="form-label" for="form2Example0">Username</label>
+    <label className="form-label" htmlFor="form2Example0">Username</label>
   </div>
 
   <div className="form-outline mb-4">
     <input type="email" id="form2Example1" className="form-control" />
-    <label className="form-label" for="form2Example1">Email address</label>
+    <label className="form-label" htmlFor="form2Example1">Email address</label>
   </div>
 
   <div className="form-outline mb-4">
     <input type="password" id="form2Example2" className="form-control" />
-    <label className="form-label" for="form2Example2">Password</label>
+    <label className="form-label" htmlFor="form2Example2">Password</label>
   </div>
   <div className="form-outline mb-4">
     <input type="password" id="form2Example" className="form-control" />
-    <label className="form-label" for="form2Example3">Re-Enter Password</label>
+    <label className="form-label" htmlFor="form2Example3">Re-Enter Password</label>
   </div>
 
   <button type="button" className="btn btn-success">Create Account!</button>
