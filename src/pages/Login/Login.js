fc3d42e858de6a5190a2c0b92c83c22922a7f738
Justin Stendara <jnstendara@gmail.com>
2023-03-17 20:47:02 -0500

    Added login as a user created on the website.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,6 +4,7 @@ import firebase from '../../../firebase/clientApp.js';
 import facebookLogin from './facebookLogin.js';
 import googleLogin from './googleLogin.js';
 import CreateUser from './newUser.js';
+import LoginUser from './loginUser.js';
 
 export default function Login(props) {
   return (
@@ -11,6 +12,7 @@ export default function Login(props) {
       <p onClick={googleLogin} className='authentication'>Google</p>
       <p onClick = {facebookLogin} className='authentication'>Facebook</p>
       <p onClick= {() => {CreateUser('jnstrendara@hotmail.com', 'asdfasdfasdf', 'justin8912');}}>Create new account</p>
+      <p onClick = {() => {LoginUser('jnstrendara@hotmail.com', 'asdfasdfasdf')}}>Login</p>
     </>
   )
 }
--- src/pages/Login/newUser.js
+++ src/pages/Login/newUser.js
@@ -2,19 +2,16 @@ import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase
 
 export default function createUser(email, password, userName) {
   const auth = getAuth();
-  console.log(password)
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in
       const user = userCredential.user;
       return updateProfile(user, {
         displayName: userName
-      })
-      // ...
+      });
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      // ..
+      return errorMessage;
     });
 }
