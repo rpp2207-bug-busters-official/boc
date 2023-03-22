86f01b999a8ed9e2fdede166fb43fe84b4752e3e
Andre Ngo <andrengo94@gmail.com>
2023-03-21 19:49:45 -0700

    useEffect for activity list

--- src/pages/ActivityList/ActivityList.js
+++ src/pages/ActivityList/ActivityList.js
@@ -1,5 +1,5 @@
 import Activity from './Activity.js';
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default function ActivityList(props) {
@@ -10,24 +10,32 @@ export default function ActivityList(props) {
   var queryURL = 'https://cors-anywhere.herokuapp.com/';
   const yelpAPI = process.env.NEXT_PUBLIC_YELP_KEY;
 
-const getNearbyActivities = async () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${yelpAPI}`
-    }
+  const getNearbyActivities = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${yelpAPI}`
+      }
+    };
+
+     fetch(`${queryURL}https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&radius=400&categories=&locale=en_US&open_now=true&sort_by=distance&device_platform=mobile-generic&limit=5`, options)
+       .then(response => response.json())
+       .then(response => {setActivities(response.businesses)})
+       .catch(err => console.error(err));
   };
 
-  fetch(`${queryURL}https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&radius=400&categories=&locale=en_US&open_now=true&sort_by=distance&device_platform=mobile-generic&limit=5`, options)
-    .then(response => response.json())
-    .then(response => {setActivities(response.businesses)})
-    .catch(err => console.error(err));
-  };
+  useEffect(() => {
+    getNearbyActivities()
+      .then(() => {
+        console.log('latitude', latitude)
+      })
+  }, [latitude])
+
+
   return (
     <div className="activity-list">
       Activity List
-      <button onClick={getNearbyActivities}>Make API Call</button>
       {activities.map((activity) => {
         return (
           <Activity action={activity} key={activity.id}/>
