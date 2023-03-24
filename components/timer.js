db6009e1005c447332d9bf3fd67d5342f8d79057
Josh Krebs <joddykrebs4@gmail.com>
2023-03-23 17:06:29 -0700

    added custom charge form as well as notify selection

--- components/timer.js
+++ components/timer.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
 
 const Timer = () => {
 
@@ -8,12 +9,24 @@ const Timer = () => {
   const [ hours, setHours ] = useState(0);
   const [ clock, setClock ] = useState("");
 
-  const [show, setShow] = useState(false);
+  const [ show, setShow ] = useState(false);
+
+  const [ email, inputEmail ] = useState(false);
+
+  const [ chargeType, setChargeType ] = useState("default");
+
+  const [ running, setRunning ] = useState(false);
 
 
   const startCharge = () => {
 
-    timer(5);
+    setRunning(!running);
+
+    if (chargeType === "default") {
+      timer(1800);
+    } else {
+      console.log('trying to run custom')
+    }
 
   }
 
@@ -54,29 +67,89 @@ const Timer = () => {
     return ret;
   }
 
+  const selectOption = (e) => {
+    // console.log(e)
+    console.log(e.target.value)
+    setChargeType(e.target.value)
+
+  }
 
   return (
     <div>
-     {
+      <div style={{paddingBottom: "1rem"}}>Select charge type: </div>
+
+    {
+      running ?
+
+      <div>
+        <br></br>
+        <span>Timer: {clock}</span>
+        <br></br>
+      </div>
+      :
+      null
+
+    }
+
+    {
       show ?
 
       <Alert variant="danger" onClose={() => setShow(false)} dismissible>
         <Alert.Heading>Your charge is done</Alert.Heading>
         <p>
-          Your 30 minute charge time has been reached.
+          Your charge time has been reached.
         </p>
       </Alert>
       :
-      <div>
-        <span>Timer: {clock}</span>
-        <br></br>
 
-      <button onClick={() => startCharge(5)} >Start Charge</button>
-      </div>
+      <Form.Select onChange={selectOption} aria-label="Default select example" value={chargeType}>
+        <option value="default">30 min charge</option>
+        <option value="custom">Custom Charge</option>
+      </Form.Select>
 
 
       }
 
+    {
+      chargeType === "custom" ?
+
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Battery Capacity:</Form.Label>
+          <Form.Control type="capacity" placeholder="3.7 kWh" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Current Charge %: </Form.Label>
+          <Form.Control type="capacity" placeholder="50%" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Charge Power: </Form.Label>
+          <Form.Control type="capacity" placeholder="4 kw" />
+        </Form.Group>
+      </Form>
+      :
+      null
+    }
+
+
+      <Form.Check label={"notify me when done charging"} onClick={() => inputEmail(!email)}/>
+
+      {email ?
+
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+        </Form>
+        :
+        null
+      }
+
+      <button onClick={startCharge}>Start Charge</button>
     </div>
   )
 }
