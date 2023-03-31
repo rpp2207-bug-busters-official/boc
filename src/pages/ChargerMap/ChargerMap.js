cc91f5b74bc85d41cb787d031c8fc9e0b1c5b4c9
hutong <hutong@gmail.com>
2023-03-31 13:21:09 -0500

    merge pull

--- src/pages/ChargerMap/ChargerMap.js
+++ src/pages/ChargerMap/ChargerMap.js
@@ -24,7 +24,7 @@ export default function ChargerMap(props) {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-100.000000);
-  const [lat, setLat] = useState(3838.5);
+  const [lat, setLat] = useState(38.5);
   const [zoom, setZoom] = useState(3);
   const [activitiesOpened, setActivitiesOpened] = useState(false);
   const [filters, setFilters] = useState({
